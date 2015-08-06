2f773e44dd9dcd5893c827d0d706fb34e0fd75a2
ADHORRIG <adhorrig@gmail.com>
2015-08-06 20:08:10 +0100

    Toast notification
    Toast notification added to confirm that a button within settings has
    been clicked.

--- MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MyActivity extends ActionBarActivity {
@@ -19,10 +21,19 @@ public class MyActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void openSearch(){
+        Toast.makeText(this, "Settings button pressed", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openSettings(){
+        Toast.makeText(this, "Settings button pressed", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -30,14 +41,16 @@ public class MyActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /** Called when the user clicks the Send button */
